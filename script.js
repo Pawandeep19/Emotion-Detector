06428ec7886a7f086d48b2f1d2bd729ec5563a38
Pawandeep singh <sirpawandeep25@gmail.com>
2021-01-02 13:42:31 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const video = document.getElementById("video")
 var contentarea=document.querySelector(".content")
-var hd=document.querySelector(".head")
-var h2=document.querySelector("h2");
+var mp=document.querySelector(".mainpage")
+var messDis=document.querySelector(".messDis");
+var imgcolor=document.querySelector(".imgbox");
+var li=document.getElementsByTagName("li");
+
 
 
 
@@ -45,24 +48,31 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     //console.log(detections[0].expressions);
     if(detections[0].expressions.neutral>0.5){
-      h2.textContent="Boring";
+      messDis.innerHTML="BORING <i class='far fa-meh-blank'></i>";
+      li[0].innerHTML="BORING <i class='fas fa-check'></i>";
+      imgcolor.style.background="linear-gradient(to right, #434343, #000000)";
     }
     else if(detections[0].expressions.angry>0.2){
-      h2.textContent="Angry";
+      messDis.innerHTML="ANGRY <i class='far fa-angry'></i>";
+      li[1].innerHTML="ANGRY <i class='fas fa-check'></i>";
+      imgcolor.style.background="linear-gradient(to right, #0a0a0b 0%, #6a0b0b 100%)";
     }
     else if(detections[0].expressions.surprised>0.8){
-      h2.textContent="Surprised";
+      messDis.innerHTML="Surprised <i class='far fa-surprise'></i>";
+      li[2].innerHTML="SURPRISED <i class='fas fa-check'></i>";
+      imgcolor.style.background=" linear-gradient(to right, #222022 0%, #0f0d68 100%)";
     }
     else if(detections[0].expressions.sad>0.1){
-      h2.textContent="Sad";
+      messDis.innerHTML="Sad <i class='far fa-frown'></i>";
+      li[3].innerHTML="SAD <i class='fas fa-check'></i>";
+      imgcolor.style.background=" linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,26,1) 100%, rgba(0,212,255,1) 100%)";
     }
     if(detections[0].expressions.happy>0.7){
-        h2.textContent="Happy";
-        console.log("pawan");
+        console.log("user happy");
         contentarea.classList.remove("invisible");
         contentarea.classList.add("visible");
-        hd.classList.add("invisiblepage");
-        hd.classList.remove("visiblepage");
+        mp.classList.add("invisible");
+        mp.classList.remove("visible");
         MediaStream.stop();
         window.clearInterval(interval);
         interval = null;
